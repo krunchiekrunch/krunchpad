4915e638b5c18d2f1c0dc4bf14b5c207b7deb5f4
krunch <75719839+krunchiekrunch@users.noreply.github.com>
2025-11-16 15:08:36 +0000

    rotary encoder works, disabled neopixel

--- firmware/KMK/main.py
+++ firmware/KMK/main.py
@@ -2,7 +2,6 @@ import board
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import MatrixScanner
 from kmk.keys import KC
-from kmk.modules.encoder import EncoderHandler
 from kmk.scanners import DiodeOrientation
 from kmk.extensions.display import Display, TextEntry
 from kmk.extensions.display.ssd1306 import SSD1306
@@ -10,12 +9,17 @@ from kmk.extensions.RGB import RGB, AnimationModes
 from kmk.modules.macros import Macros, Press, Release, Tap
 from kmk.modules.mouse_keys import MouseKeys
 from kmk.extensions.media_keys import MediaKeys
+from kmk.modules.encoder import EncoderHandler
 
 keyboard = KMKKeyboard()
+
 macros = Macros()
 keyboard.modules.append(macros)
+mouse = MouseKeys()
 keyboard.modules.append(MouseKeys())
 keyboard.extensions.append(MediaKeys())
+encoder_handler = EncoderHandler()
+keyboard.modules.append(encoder_handler)
 
 # matrix
 keyboard.row_pins = (board.D10, board.D9, board.D8)
@@ -24,17 +28,17 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 keyboard.keymap = [
     [
-        KC.Q, KC.W, KC.MW_UP, KC.MW_DN,
-        KC.T, KC.S, KC.D, KC.H,
-        KC.COMMA, KC.DOT, KC.C, KC.PGUP,
+        KC.Q,     KC.W,   KC.MW_UP, KC.MW_DN,
+        KC.T,     KC.S,   KC.D,     KC.H,
+        KC.COMMA, KC.DOT, KC.C,     KC.PGUP,
     ]
 ]
 
 # volume knob
-encoder = EncoderHandler()
-encoder.pins = ((board.D7, board.D6),)
-encoder.map = [((KC.VOLU, KC.VOLD))]  # cw = vol up, ccw = vol down
-keyboard.modules.append(encoder)
+encoder_handler.pins = (
+    (board.D6, board.D7),
+    )
+encoder_handler.map = [((KC.VOLD, KC.VOLU),)]
 
 # 128x32 OLED display
 display = Display(
@@ -60,7 +64,8 @@ neopixel = RGB(
     val_default=100, # brightness/value
     animation_mode=AnimationModes.STATIC,
 )
-keyboard.extensions.append(neopixel)
+# keyboard.extensions.append(neopixel)
 
 if __name__ == '__main__':
     keyboard.go()
+
