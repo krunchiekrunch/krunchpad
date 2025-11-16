c735dd55cf8cd2e838cc8d8ea0e1a03c2b7b15d5
krunch <75719839+krunchiekrunch@users.noreply.github.com>
2025-11-16 03:54:58 +0000

    updated keymap, neopixel

--- firmware/KMK/main.py
+++ firmware/KMK/main.py
@@ -7,9 +7,15 @@ from kmk.scanners import DiodeOrientation
 from kmk.extensions.display import Display, TextEntry
 from kmk.extensions.display.ssd1306 import SSD1306
 from kmk.extensions.RGB import RGB, AnimationModes
-from kmk.extensions.LED import LED
+from kmk.modules.macros import Macros, Press, Release, Tap
+from kmk.modules.mouse_keys import MouseKeys
+from kmk.extensions.media_keys import MediaKeys
 
 keyboard = KMKKeyboard()
+macros = Macros()
+keyboard.modules.append(macros)
+keyboard.modules.append(MouseKeys())
+keyboard.extensions.append(MediaKeys())
 
 # matrix
 keyboard.row_pins = (board.D10, board.D9, board.D8)
@@ -18,9 +24,9 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 keyboard.keymap = [
     [
-        KC.TAB, KC.Q, KC.W, KC.E,
-        KC.LSHIFT, KC.A, KC.S, KC.D,
-        KC.LCTRL, KC.F3, KC.F5, KC.SPACE,
+        KC.Q, KC.W, KC.MW_UP, KC.MW_DN,
+        KC.T, KC.S, KC.D, KC.H,
+        KC.COMMA, KC.DOT, KC.C, KC.PGUP,
     ]
 ]
 
@@ -31,32 +37,30 @@ encoder.map = [((KC.VOLU, KC.VOLD))]  # cw = vol up, ccw = vol down
 keyboard.modules.append(encoder)
 
 # 128x32 OLED display
-
-keyboard.SCL = board.D5
-keyboard.SDA = board.D4
-
 display = Display(
     display=SSD1306(sda=board.D4, scl=board.D5),
     entries=[
         TextEntry(text="""
-hello
-world
+Q W + -    SCR Config
+T S D H     krunchpad
+< > C PU      HW v1.1
         """),
     ],
     height=32,
 )
 keyboard.extensions.append(display)
 
-# MCU neopixel
-
-mcuneopixel = RGB(
+# Onboard Neopixel
+neopixel = RGB(
     pixel_pin=board.NEOPIXEL,
     num_pixels=1,
     val_limit=100,
-    val_default=25,
-    animation_mode=AnimationModes.RAINBOW,
+    hue_default=170, # hue
+    sat_default=0, # satuation
+    val_default=100, # brightness/value
+    animation_mode=AnimationModes.STATIC,
 )
-keyboard.extensions.append(mcuneopixel)
+keyboard.extensions.append(neopixel)
 
 if __name__ == '__main__':
     keyboard.go()
