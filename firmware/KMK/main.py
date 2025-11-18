b173a6f5b3b4c199798054036544c590956032b6
krunch <75719839+krunchiekrunch@users.noreply.github.com>
2025-11-18 13:35:42 +0000

    Added multi layers, fn key

--- firmware/KMK/main.py
+++ firmware/KMK/main.py
@@ -10,44 +10,56 @@ from kmk.modules.macros import Macros, Press, Release, Tap
 from kmk.modules.mouse_keys import MouseKeys
 from kmk.extensions.media_keys import MediaKeys
 from kmk.modules.encoder import EncoderHandler
+from kmk.modules.layers import Layers
 
 keyboard = KMKKeyboard()
-
 macros = Macros()
-keyboard.modules.append(macros)
 mouse = MouseKeys()
+encoder_handler = EncoderHandler()
+keyboard.modules.append(macros)
 keyboard.modules.append(MouseKeys())
 keyboard.extensions.append(MediaKeys())
-encoder_handler = EncoderHandler()
 keyboard.modules.append(encoder_handler)
+keyboard.modules.append(Layers())
 
 # matrix
 keyboard.row_pins = (board.D10, board.D9, board.D8)
 keyboard.col_pins = (board.D0, board.D1, board.D2, board.D3)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
+_______ = KC.TRNS
+xxxxxxx = KC.NO
+Fn = KC.MO(1)
+
 keyboard.keymap = [
     [
-        KC.Q,     KC.W,   KC.MW_UP, KC.MW_DN,
-        KC.T,     KC.S,   KC.D,     KC.H,
-        KC.COMMA, KC.DOT, KC.C,     KC.PGUP,
-    ]
+        KC.Q,     KC.W,   KC.D, KC.PGUP,
+        KC.T,     KC.S,   KC.M,    KC.H,
+        KC.COMMA, KC.DOT, KC.C,      Fn,
+    ],
+    
+    [
+        KC.MUTE, KC.MPRV, KC.MPLY, KC.MNXT, 
+        xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
+        xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
+    ],
 ]
 
 # volume knob
-encoder_handler.pins = (
-    (board.D6, board.D7),
-    )
-encoder_handler.map = [((KC.VOLD, KC.VOLU),)]
+encoder_handler.pins = ((board.D6, board.D7),)
+encoder_handler.map = [
+    ((KC.MW_DN, KC.MW_UP),), # Mouse wheel zooming
+    ((KC.VOLD, KC.VOLU),),   # Volume control
+    ]
 
 # 128x32 OLED display
 display = Display(
     display=SSD1306(sda=board.D4, scl=board.D5),
     entries=[
         TextEntry(text="""
-Q W + -    SCR Config
-T S D H     krunchpad
-< > C PU      HW v1.1
+Q W D PU     ZOOM/VOL
+T S M H     krunchpad
+< > C Fn      HW v1.1
         """),
     ],
     height=32,
@@ -60,12 +72,11 @@ neopixel = RGB(
     num_pixels=1,
     val_limit=100,
     hue_default=170, # hue
-    sat_default=0, # satuation
-    val_default=100, # brightness/value
+    sat_default=60, # satuation
+    val_default=45, # brightness/value
     animation_mode=AnimationModes.STATIC,
 )
 # keyboard.extensions.append(neopixel)
 
 if __name__ == '__main__':
     keyboard.go()
-
